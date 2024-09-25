4131f8f8d651fa202e99766613362300ffaa711c
casey.chen <casey.chen@rock-chips.com>
2024-09-25 17:11:59 +0800

    [C2RKMpiDec]: Fix dataspace unsupported issue
    kColorStandardBT470M is platform unsupported format
    
    error log:
    E mali_config_interface: Unsupported dataspace standard (524288)
    
    Type: Fix
    Redmine ID: #507411
    Associated modifications: N/A
    Test: N/A
    Version: 1.14.6_[3]
    
    Signed-off-by: casey.chen <casey.chen@rock-chips.com>
    Change-Id: I7e63595dd80b0b7e2934e291b8aa9a1378d59ef7

--- component/video/C2RKMpiDec.cpp
+++ component/video/C2RKMpiDec.cpp
@@ -1612,6 +1612,10 @@ void C2RKMpiDec::setDefaultCodecColorAspectsIfNeeded(ColorAspects &aspects) {
             aspects.mMatrixCoeffs == CA::MatrixSMPTE240M) {
             aspects.mMatrixCoeffs = CA::MatrixBT601_6;
         }
+    } else if (aspects.mPrimaries == CA::PrimariesBT470_6M) {
+        // got platform unsupport BT470_6M format, use BT601_625 instead
+        aspects.mPrimaries == CA::PrimariesBT601_6_625;
+        aspects.mMatrixCoeffs == CA::MatrixBT601_6;
     }
 }
 
